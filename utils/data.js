540a87c4978fd026cfac912e819ef9197e006be5
Jasur <zarrukhjurakulov474@gmail.com>
2022-02-07 12:02:44 +0500

    feat: product list

---
+++ utils/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products: [
+    {
+      name: "Free Shirt",
+      category: "Shirts",
+      image:
+        "https://cdn.pixabay.com/photo/2013/07/13/14/07/apparel-162180_1280.png",
+      price: 70,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Fit Shirt",
+      category: "Shirts",
+      image:
+        "https://cdn.pixabay.com/photo/2016/03/25/09/04/t-shirt-1278404_1280.jpg",
+      price: 80,
+      brand: "Adidas",
+      rating: 4.2,
+      numReviews: 10,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Golf Pants",
+      category: "Pants",
+      image:
+        "https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg",
+      price: 90,
+      brand: "Raymond",
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Free Shirt",
+      category: "Shirts",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/27/05/33/trousers-2685231_1280.jpg",
+      price: 90,
+      brand: "Oliver",
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: "Smart looking pants",
+    },
+    {
+      name: "Fit Pants",
+      category: "Shirts",
+      image:
+        "https://cdn.pixabay.com/photo/2016/11/29/09/41/bag-1868758_1280.jpg",
+      price: 95,
+      brand: "Zara",
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: "A popular pants",
+    },
+    {
+      name: "Classic Pants",
+      category: "Pants",
+      image:
+        "https://media.istockphoto.com/photos/mans-legs-picture-id527236518?s=612x612",
+      price: 75,
+      brand: "Casely",
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: "A popular pants",
+    },
+  ],
+};
+
+export default data;
